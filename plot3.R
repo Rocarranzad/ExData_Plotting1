477ea7bc871cb38c44a9da1e9c3c9c4f9101b8cb
Rocarranzad <dr.rogeliocarranza@gmail.com>
2025-05-08 19:53:04 +0200

    Commit some changes to the plot3.R script and plot3.png. The script was not creating the plot correctly because plot had not been called.

--- plot3.R
+++ plot3.R
@@ -24,8 +24,15 @@ df <- subset(data, dttm >= as.Date("2007-02-01 00:00")
 # Create a linear plot for the three energy sub metering measurements along
 # the two-day period we observe for this project.
 png("plot3.png", width = 480, height = 480)
+plot(df$dttm, df$Sub_metering_1, 
+     type = "n", 
+     xlab = "", 
+     ylab = "Energy sub metering")
 lines(df$dttm, df$Sub_metering_1, col = "black")
 lines(df$dttm, df$Sub_metering_2, col = "red")
 lines(df$dttm, df$Sub_metering_3, col = "blue")
-legend("topright", legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty = 1, col = c("black", "red", "blue"))
+legend("topright", 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+       lty = 1, 
+       col = c("black", "red", "blue"))
 dev.off()
